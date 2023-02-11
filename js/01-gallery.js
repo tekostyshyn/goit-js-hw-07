53578d3f7db58b045187e6f873a60ab805c8f1d7
tekostyshyn <tekostyshyn@gmail.com>
2023-02-11 10:46:35 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
 const galleryEl = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
-galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup)
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
 
 function createGalleryMarkup(gallery) {
-  return gallery.map(({ preview, description, original }) => {
-    return `<div class="gallery__item">
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
         <a class="gallery__link" href=${original}>
           <img
             class="gallery__image"
@@ -18,9 +18,15 @@ function createGalleryMarkup(gallery) {
           />
         </a>
         </div>`;
-  }).join('');
+    })
+    .join("");
 }
 
+galleryEl.addEventListener("click", onGalleryImgClick);
 
-
-
+function onGalleryImgClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+}

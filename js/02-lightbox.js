7a60e32d6b74f9a5b8e9beaccc7f5e0625fe9c29
tekostyshyn <tekostyshyn@gmail.com>
2023-02-12 16:10:08 +0200

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<li class="gallery__item"><a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+    </a></li>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: 'alt', captionDelay: 250
+});
+
